d8f2d077c9cecdad25d201d0f82ef4fe4888ac53
Kasem Arnontavilas <mrtomyum@gmail.com>
2016-11-12 15:10:20 +0700

    add .gitignore

---
+++ controller/order.go
@@ -0,0 +1,6 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func PostOrder(ctx *gin.Context) {
+}
--- controller/router.go
+++ controller/router.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 func Router(r *gin.Engine) *gin.Engine {
 	r.GET("/", MenuIndex)
 	r.GET("/item/:id", GetItemByMenu)
+	r.POST("/order", PostOrder)
 	return r
 }
 
