59390f92fa3a8b6c12678751a3c707b9afc8b011
Kasem Arnontavilas <mrtomyum@gmail.com>
2016-11-17 23:37:37 +0700

    Add wsView, wsDevice for websocket listening  and serve.

--- controller/router.go
+++ controller/router.go
@@ -13,8 +13,11 @@ func Router(r *gin.Engine) *gin.Engine {
 	r.GET("/list", GetMenu)
 	r.GET("/menu/:id/item", GetItemByMenuId)
 
-	r.GET("/ws", func(c *gin.Context) {
+	r.GET("/view", func(c *gin.Context) {
 		wsView(c.Writer, c.Request)
+	})
+	r.GET("/device", func(c *gin.Context) {
+		wsDevice(c.Writer, c.Request)
 	})
 	return r
 }
@@ -39,6 +42,23 @@ func wsView(w http.ResponseWriter, r *http.Request) {
 			//select {
 			//
 			//}
+			conn.WriteMessage(t, msg)
+		}
+	}()
+}
+
+func wsDevice(w http.ResponseWriter, r *http.Request) {
+	conn, err := wsUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Failed to set websocket upgrade: %+v", err)
+		return
+	}
+	go func() {
+		for {
+			t, msg, err := conn.ReadMessage()
+			if err != nil {
+				break
+			}
 			conn.WriteMessage(t, msg)
 		}
 	}()
