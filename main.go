4a295612b8f03bab159f795005f658710da8537c
nathaphol <nathaphol008@gmail.com>
2016-11-21 11:49:11 +0700

    Merge branch 'master' of C:\gocode\src\github.com\mrtomyum\paybox_terminal with conflicts.

--- main.go
+++ main.go
@@ -3,17 +3,15 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	c "github.com/mrtomyum/paybox_terminal/controller"
+	_ "net/http"
 )
 
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("view/**/*.tpl")
 	r.Static("/public", "./view/public")
-	r.Static("/js", "./view/public/js")
-	r.Static("/css", "./view/public/css")
-	r.Static("/img", "./view/public/img")
 	app := c.Router(r)
-	app.Run(":8888")
+	app.Run(":8080")
 }
 
 
