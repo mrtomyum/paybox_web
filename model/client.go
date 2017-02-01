f9074119148793d1ab70ed4cc8d57c0a5f97153d
Kasem Arnontavilas <mrtomyum@gmail.com>
2017-02-01 23:33:23 +0700

    แก้ไขให้หลังจาก Web Client เชื่อมต่อแล้วให้ Host ​Dial WS ไปยัง Dev WS ที่ port :9999 ทันที

--- model/client.go
+++ model/client.go
@@ -5,6 +5,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"log"
+	"net/url"
+	"time"
+	"os"
+	"os/signal"
 )
 
 type Client struct {
@@ -91,4 +95,61 @@ func (c *Client) DevEvent() {
 	default:
 		log.Println("event cannot find function/message=", c.Msg)
 	}
+}
+
+func CallDev() error {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	u := url.URL{Scheme:"ws", Host:"localhost:9999", Path: "/ws"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+	done := make(chan struct{})
+	m := &Message{}
+
+	go func() {
+		defer c.Close()
+		defer close(done)
+		for {
+			err := c.ReadJSON(&m)
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Panicf("recev: %s", m)
+		}
+	}()
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case t := <-ticker.C:
+			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+			if err != nil {
+				log.Println("write:", err)
+				return err
+			}
+		case <-interrupt:
+			log.Println("interrupt")
+			// To cleanly close a connection, a client should send a close
+			// frame and wait for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return err
+			}
+				select {
+				case <-done:
+				case <-time.After(time.Second):
+				}
+			c.Close()
+			return nil
+		}
+	}
+	return nil
 }
