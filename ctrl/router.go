e5bfc8eb5a54d700be7243d714a2ac69ab721432
Kasem Arnontavilas <mrtomyum@gmail.com>
2017-02-03 19:36:15 +0700

    ลืมลบบรรทัด

--- ctrl/router.go
+++ ctrl/router.go
@@ -6,37 +6,37 @@ import (
 )
 
 func Router(r *gin.Engine) *gin.Engine {
-	// for Static HTML
+	// for Static HTML template
 	r.LoadHTMLGlob("view/**/*.tpl")
 
 	// for test run disable this if production
-	r.Static("/html", "view/html")
-	r.Static("/js", "view/public/js")
-	r.Static("/css", "view/public/css")
-	r.Static("/img", "view/public/img")
-	r.Static("/json", "view/public/json")
-	r.Use(static.Serve("/", static.LocalFile("view", true)))
+	//r.Static("/html", "./view/html")
+	//r.Static("/js", "./view/public/js")
+	//r.Static("/css", "./view/public/css")
+	//r.Static("/img", "./view/public/img")
+	//r.Static("/json", "./view/public/json")
+	//r.Use(static.Serve("/", static.LocalFile("view", true)))
 
-	// for deploy production
-	//r.Static("/js", "/opt/paybox/web_service/view/public/js")
-	//r.Static("/css", "/opt/paybox/web_service/view/public/css")
-	//r.Static("/img", "/opt/paybox/web_service/view/public/img")
-	//r.Static("/json", "/opt/paybox/web_service/view/public/json")
-	//r.Use(static.Serve("/", static.LocalFile("/opt/paybox/web_service/view", true)))
+	// Absolute path static file for deploy production.
+	r.Static("/js", "/opt/paybox/web_service/view/public/js")
+	r.Static("/css", "/opt/paybox/web_service/view/public/css")
+	r.Static("/img", "/opt/paybox/web_service/view/public/img")
+	r.Static("/json", "/opt/paybox/web_service/view/public/json")
+	r.Use(static.Serve("/", static.LocalFile("/opt/paybox/web_service/view", true)))
 
-	// for WebService endpoint call data in JSON
+	// WebService endpoint for web UI
 	r.GET("/menu", GetMenu)
 	r.GET("/menu/:id/", GetItemsByMenuId)
 	r.GET("/item/:id", GetItemById)
 	r.POST("/sale", NewSale)
 
-	// for WebSocket Connection
+	// WebSocket endpoint for web UI
 	r.GET("/web", func(c *gin.Context) {
 		ServWeb(c.Writer, c.Request)
 	})
-	r.GET("/dev", func(c *gin.Context) {
-		ServDev(c.Writer, c.Request)
-	})
+	//r.GET("/dev", func(c *gin.Context) {
+	//	ServDev(c.Writer, c.Request)
+	//})
 
 
 	return r
