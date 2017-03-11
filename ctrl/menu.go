a25b5fd18c729b2e89f95e2f18668c48a7e82f6c
Kasem Arnontavilas <mrtomyum@gmail.com>
2017-03-11 14:27:51 +0700

    เพิ่มสั่งปิด Payment device ซ้ำ กรณีหลุดกลับมาหน้า Menu แบบผิดขั้นตอน

--- ctrl/menu.go
+++ ctrl/menu.go
@@ -14,5 +14,7 @@ func GetMenu(ctx *gin.Context) {
 		log.Println(err.Error())
 		ctx.JSON(http.StatusNotFound, err.Error())
 	}
+	model.CA.Stop()
+	model.BA.Stop()
 	ctx.JSON(http.StatusOK, langs)
 }
