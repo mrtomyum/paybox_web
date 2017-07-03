f0fc27599c3560969f6cb2c2e9e0b2f7becaec65
nathaphol <nathaphol008@gmail.com>
2017-07-03 10:53:31 +0700

    edit lender voice and refacter code with clean

--- view2/public/js/setting.js
+++ view2/public/js/setting.js
@@ -8,15 +8,10 @@ function AddM(id) {
     var remove = document.getElementsByClassName('removeM');
     var cnt = document.getElementsByClassName('C');
     var price = document.getElementsByClassName('tc');
-
     add[id].style.display = 'block';
     remove[id].style.display = 'block';
-
     var data = parseInt(cnt[id].innerText) + 1;
     var ct = data + ",000";
-
-    ///console.log('add = '+id+" ,cnt = "+data);
-
     cnt[id].innerHTML = data;
     price[id].innerHTML = ct;
     sumadd();
@@ -39,19 +34,15 @@ function RemoveM(id, text) {
     }
     cnt[id].innerHTML = data;
     price[id].innerHTML = ct;
-
     sumadd();
-    //console.log('add = '+id+" ,cnt = "+data);
 }
 
 function sumadd() {
     var price = document.getElementsByClassName('tc');
-
     var total = 0;
     for (var i = 0; i < price.length; i++) {
         total += parseInt(price[i].innerText);
     }
-
     if (total == 0) {
         addMN.innerHTML = "฿ 0";
     } else {
@@ -65,13 +56,8 @@ function sumtotal() {
     var addMN = document.getElementById('addMN');
     var scMN = document.getElementById('scMN');
     var ttMN = document.getElementById('ttMN');
-
     var pri = addMN.innerText.split(" ");
     var sc = scMN.innerText.split(" ");
-
     var totaltext = parseInt(sc[1]) + parseInt(pri[1]);
-    console.log(parseInt(pri[1]));
-
     ttMN.innerHTML = "฿ " + totaltext + ",000";
-
 }
